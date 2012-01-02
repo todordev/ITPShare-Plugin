bc0e22824542fb60fa1b783ee25d0a29bf026722
Todor Iliev <todor@itprism.com>
2012-01-02 15:31:53 +0200

    Added a version for Joomla! 1.5

--- itpshare.php
+++ itpshare.php
@@ -20,23 +20,16 @@ jimport('joomla.plugin.plugin');
  *
  * @package		ITPrism Plugins
  * @subpackage	Social
+ * @since 		1.5
  */
 class plgContentITPShare extends JPlugin {
     
     private $fbLocale = "en_US";
-    private $currentView = "";
     
-    /**
-     * Constructor
-     *
-     * @param object $subject The object to observe
-     * @param array  $config  An optional associative array of configuration settings.
-     * Recognized key values include 'name', 'group', 'params', 'language'
-     * (this list is not meant to be comprehensive).
-     */
-    public function __construct(&$subject, $config = array()) {
-        parent::__construct($subject, $config);
-      
+    public function __construct($subject, $params){
+        
+        parent::__construct($subject, $params);
+    
         if($this->params->get("fbDynamicLocale", 0)) {
             $lang = JFactory::getLanguage();
             $locale = $lang->getTag();
@@ -44,20 +37,21 @@ class plgContentITPShare extends JPlugin {
         } else {
             $this->fbLocale = $this->params->get("fbLocale", "en_US");
         }
-        
+
     }
     
     /**
      * Add social buttons into the article
      *
-	 * @param	string	The context of the content being passed to the plugin.
-	 * @param	object	The article object.  Note $article->text is also available
-	 * @param	object	The article params
-	 * @param	int		The 'page' number
-	 */
-    public function onContentPrepare($context, &$article, &$params, $page = 0) {
-
-        if (!$article) { return; };            
+     * Method is called by the view
+     *
+     * @param   string  The context of the content being passed to the plugin.
+     * @param   object  The content object.  Note $article->text is also available
+     * @param   object  The content params
+     * @param   int     The 'page' number
+     * @since   1.6
+     */
+    public function onPrepareContent(&$article, &$params, $limitstart){
         
         $app =& JFactory::getApplication();
         /* @var $app JApplication */
@@ -66,7 +60,7 @@ class plgContentITPShare extends JPlugin {
             return;
         }
         
-        $doc     = JFactory::getDocument();
+        $doc   = JFactory::getDocument();
         /* @var $doc JDocumentHtml */
         $docType = $doc->getType();
         
@@ -77,54 +71,88 @@ class plgContentITPShare extends JPlugin {
         
         $currentOption = JRequest::getCmd("option");
         
-        if( ($currentOption != "com_content") OR !isset($this->params)) {
+        if(($currentOption != "com_content") OR !isset($article) OR empty($article->id) OR !isset($this->params)) {
             return;            
         }
         
-        $this->currentView  = JRequest::getCmd("view");
+        // Generate buttons
+        $buttons    = $this->getContent($article, $params);
+        $position   = $this->params->get('position');
+        
+        switch($position){
+            case 1:
+                $article->text = $buttons . $article->text;
+                break;
+            case 2:
+                $article->text = $article->text . $buttons;
+                break;
+            default:
+                $article->text = $buttons . $article->text . $buttons;
+                break;
+        }
+        
+        return;
+    }
+    
+    
+    /**
+     * Generate content
+     * @param   object      The article object.  Note $article->text is also available
+     * @param   object      The article params
+     * @return  string      Returns html code or empty string.
+     */
+    private function getContent(&$article, &$params){
+        
+        $doc         = JFactory::getDocument();
+        $currentView = JRequest::getWord("view");
+        
+        // Check where we are able to show buttons?
+        $showInArticles     = $this->params->get('showInArticles');
+        $showInCategories   = $this->params->get('showInCategories');
+        $showInSections     = $this->params->get('showInSections');
+        $showInFrontPage    = $this->params->get('showInFrontPage');
         
         /** Check for selected views, which will display the buttons. **/   
         /** If there is a specific set and do not match, return an empty string.**/
-        $showInArticles     = $this->params->get('showInArticles');
-        
-        if(!$showInArticles AND (strcmp("article", $this->currentView) == 0)){
-            return;
+        if(!$showInArticles AND (strcmp("article", $currentView) == 0)){
+            return "";
         }
         
-        // Check for category view
-        $showInCategories   = $this->params->get('showInCategories');
-        
-        if(!$showInCategories AND (strcmp("category", $this->currentView) == 0)){
-            return;
+        if(!$showInCategories AND (strcmp("category", $currentView) == 0)){
+            return "";
         }
         
-        if($showInCategories AND ($this->currentView == "category")) {
-            $articleData        = $this->getArticle($article);
-            $article->id        = $articleData['id'];
-            $article->catid     = $articleData['catid'];
-            $article->title     = $articleData['title'];
-            $article->slug      = $articleData['slug'];
-            $article->catslug   = $articleData['catslug'];
+        if(!$showInSections AND (strcmp("section", $currentView) == 0)){
+            return "";
         }
         
-        if(empty($article->id)) {
-            return;            
+        if(!$showInFrontPage AND (strcmp("frontpage", $currentView) == 0)){
+            return "";
         }
         
+        // Exclude categories
+        $excludedCats = $this->params->get('excludeCats');
+        if(!empty($excludedCats)){
+            $excludedCats = explode(',', $excludedCats);
+        }
+        settype($excludedCats, 'array');
+        JArrayHelper::toInteger($excludedCats);
+        
+        // Exclude sections
+        $excludeSections = $this->params->get('excludeSections');
+        if(!empty($excludeSections)){
+            $excludeSections = explode(',', $excludeSections);
+        }
+        settype($excludeSections, 'array');
+        JArrayHelper::toInteger($excludeSections);
+        
+        // Exclude articles
         $excludeArticles = $this->params->get('excludeArticles');
         if(!empty($excludeArticles)){
             $excludeArticles = explode(',', $excludeArticles);
         }
         settype($excludeArticles, 'array');
         JArrayHelper::toInteger($excludeArticles);
-        
-        // Exluded categories
-        $excludedCats           = $this->params->get('excludeCats');
-        if(!empty($excludedCats)){
-            $excludedCats = explode(',', $excludedCats);
-        }
-        settype($excludedCats, 'array');
-        JArrayHelper::toInteger($excludedCats);
         
         // Included Articles
         $includedArticles = $this->params->get('includeArticles');
@@ -136,53 +164,25 @@ class plgContentITPShare extends JPlugin {
         
         if(!in_array($article->id, $includedArticles)) {
             // Check exluded places
-            if(in_array($article->id, $excludeArticles) OR in_array($article->catid, $excludedCats)){
-                return;
+            if(in_array($article->catid, $excludedCats) OR in_array($article->sectionid, $excludeSections) OR in_array($article->id, $excludeArticles)){
+                return "";
             }
         }
-            
-        // Generate content
-		$content      = $this->getContent($article, $params);
-        $position     = $this->params->get('position');
         
-        switch($position){
-            case 1:
-                $article->text = $content . $article->text;
-                break;
-            case 2:
-                $article->text = $article->text . $content;
-                break;
-            default:
-                $article->text = $content . $article->text . $content;
-                break;
-        }
-        
-        return;
-    }
-    
-    /**
-     * Generate content
-     * @param   object      The article object.  Note $article->text is also available
-     * @param   object      The article params
-     * @return  string      Returns html code or empty string.
-     */
-    private function getContent(&$article, &$params){
-        
-        $doc   = JFactory::getDocument();
-        /* @var $doc JDocumentHtml */
-
         if($this->params->get("loadCss")) {
             $doc->addStyleSheet(JURI::root() . "plugins/content/itpshare/style.css");
         }
         
-        $url = JURI::getInstance();
-        $domain= $url->getScheme() ."://" . $url->getHost();
+        $url = JURI::base();
+        $url = new JURI($url);
+        $root= $url->getScheme() ."://" . $url->getHost();
         
-        $url = JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catslug), false);
-        $url = $domain.$url;
+        $url = JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catslug, $article->sectionid), false);
+        $url = $root.$url;
         
-        $title= htmlentities($article->title, ENT_QUOTES, "UTF-8");
+        $title  = htmlentities($article->title, ENT_QUOTES, "UTF-8");
         
+        // Start buttons box
         $html = '
         <div class="itp-share">';
         
@@ -200,47 +200,13 @@ class plgContentITPShare extends JPlugin {
         // Gets extra buttons
         $html   .= $this->getExtraButtons($this->params, $url, $title);
         
+        // End buttons box
         $html .= '
         </div>
         <div style="clear:both;"></div>
         ';
     
         return $html;
-    }
-    
-    private function getArticle(&$article) {
-        
-        $db = JFactory::getDbo();
-        
-        $query = "
-            SELECT 
-                `#__content`.`id`,
-                `#__content`.`catid`,
-                `#__content`.`alias`,
-                `#__content`.`title`,
-                `#__categories`.`alias` as category_alias
-            FROM
-                `#__content`
-            INNER JOIN
-                `#__categories`
-            ON
-                `#__content`.`catid`=`#__categories`.`id`
-            WHERE
-                `#__content`.`introtext` = " . $db->Quote($article->text); 
-        
-        $db->setQuery($query);
-        $result = $db->loadAssoc();
-        
-        if ($db->getErrorNum() != 0) {
-            JError::raiseError(500, "System error!", $db->getErrorMsg());
-        }
-        
-        if(!empty($result)) {
-            $result['slug']     = $result['alias'] ? $result['id'].':'.$result['alias'] : $result['id'];
-            $result['catslug']  = $result['category_alias'] ? $result['catid'].':'.$result['category_alias'] : $result['catid'];
-        }
-        
-        return $result;
     }
     
     /**
@@ -308,7 +274,7 @@ class plgContentITPShare extends JPlugin {
         return $html;
     }
     
-    /**
+	/**
      * 
      * Render the Google plus one in standart syntax
      * 
@@ -328,7 +294,7 @@ class plgContentITPShare extends JPlugin {
         
         // Load the JavaScript asynchroning
 		if($params->get("loadGooglePlusJsLib")) {
-  
+      
 $html .= '<script type="text/javascript">';
 if($language) {
    $html .= ' window.___gcfg = '.$language;
@@ -341,12 +307,12 @@ $html .= '
     var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);
   })();
 </script>';
-				}
-				
+    				}
+    				
         return $html;
     }
     
-    /**
+	/**
      * 
      * Render the Google plus one in HTML5 syntax
      * 
@@ -382,7 +348,6 @@ $html .= '
     				
         return $html;
     }
-    
     
     private function getFacebookLike($params, $url, $title){
         
@@ -582,7 +547,6 @@ href="http://digg.com/submit?url=' . rawurlencode($url) . '&amp;title=' . rawurl
             <script type="text/javascript" src="http://platform.linkedin.com/in.js"></script><script type="IN/Share" data-url="' . $url . '" data-counter="' . $params->get("linkedInType",'right'). '"></script>
             </div>
             ';
-
         }
         
         return $html;
@@ -711,49 +675,48 @@ tweetmeme_source = "' . $params->get("twitterName") . '";
     
     private function getTumblr($params, $url, $title){
             
-            $html = "";
-            if($params->get("tumblrButton")) {
-                
-                $html .= '<div class="itp-share-tbr">';
-                
-                if($params->get("loadTumblrJsLib")) {
-                    $html .= '<script type="text/javascript" src="http://platform.tumblr.com/v1/share.js"></script>';
-                }
-                
-                switch($params->get("tumblrType")) {
-                    
-                    case 1:
-                        $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:61px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_2.png\') top left no-repeat transparent;">Share on Tumblr</a>';
-                        break;
-
-                    case 2:
-                        $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:129px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_3.png\') top left no-repeat transparent;">Share on Tumblr</a>';
-                        break;
-                    case 3:
-                        $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:20px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_4.png\') top left no-repeat transparent;">Share on Tumblr</a>';
-                        break;
-                    case 4:
-                        $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:81px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_1T.png\') top left no-repeat transparent;">Share on Tumblr</a>';
-                        break;
-                    case 5:
-                        $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:61px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_2T.png\') top left no-repeat transparent;">Share on Tumblr</a>';
-                        break;
-                    case 6:
-                        $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:129px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_3T.png\') top left no-repeat transparent;">Share on Tumblr</a>';
-                        break;
-                    case 7:
-                        $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:20px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_4T.png\') top left no-repeat transparent;">Share on Tumblr</a>';
-                        break;   
-                                        
-                    default:
-                        $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:81px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_1.png\') top left no-repeat transparent;">Share on Tumblr</a>';
-                        break;
-                }
-                
-                $html .='</div>';
+        $html = "";
+        if($params->get("tumblrButton")) {
+            
+            $html .= '<div class="itp-share-tbr">';
+            
+            if($params->get("loadTumblrJsLib")) {
+                $html .= '<script type="text/javascript" src="http://platform.tumblr.com/v1/share.js"></script>';
             }
             
-            return $html;
+            switch($params->get("tumblrType")) {
+                
+                case 1:
+                    $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:61px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_2.png\') top left no-repeat transparent;">Share on Tumblr</a>';
+                    break;
+
+                case 2:
+                    $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:129px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_3.png\') top left no-repeat transparent;">Share on Tumblr</a>';
+                    break;
+                case 3:
+                    $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:20px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_4.png\') top left no-repeat transparent;">Share on Tumblr</a>';
+                    break;
+                case 4:
+                    $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:81px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_1T.png\') top left no-repeat transparent;">Share on Tumblr</a>';
+                    break;
+                case 5:
+                    $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:61px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_2T.png\') top left no-repeat transparent;">Share on Tumblr</a>';
+                    break;
+                case 6:
+                    $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:129px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_3T.png\') top left no-repeat transparent;">Share on Tumblr</a>';
+                    break;
+                case 7:
+                    $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:20px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_4T.png\') top left no-repeat transparent;">Share on Tumblr</a>';
+                    break;   
+                                    
+                default:
+                    $html .='<a href="http://www.tumblr.com/share" title="Share on Tumblr" style="display:inline-block; text-indent:-9999px; overflow:hidden; width:81px; height:20px; background:url(\'http://platform.tumblr.com/v1/share_1.png\') top left no-repeat transparent;">Share on Tumblr</a>';
+                    break;
+            }
+            
+            $html .='</div>';
         }
         
+        return $html;
+    }
 }
